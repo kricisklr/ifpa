e7c1b4397fdcf4d4c98ffbae26b1504787da2a61
Kevin Rice <k.l.rice@nasa.gov>
2014-11-03 13:38:24 -0500

    added controller default functions

--- js/controllers.js
+++ js/controllers.js
@@ -17,3 +17,38 @@ var ctrlRoutes = function ctrlRoutes($routeProvider) {
 	        redirectTo: '/'
 	    });  
 };
+
+var ifpControllers = angular.module('ifpControllers',
+    []);
+
+ifpControllers.factory('ifpData', 
+	['$http','$rootScope','$q',
+	function($http,$rootScope,$q) {
+	return {
+		getUsers: function() {
+			var promise =  $http.get('/user/getUsers').success(function(data) {	
+		    	return data;
+		  	});
+		  	
+		  	return promise;
+		},
+		getMenu: function(type) {
+			var promise =  $http.get('/user/getMenu/'+type).success(function(data) {	
+		    	return data;
+		  	});
+		  	
+		  	return promise;
+		}
+	};
+}]);
+
+ifpControllers.controller('DefaultCtrl', 
+	['$scope','$http','$rootScope','$location','users',
+  	function($scope,$http,$rootScope,$location,users) {
+  		$rootScope.actionMenu = [
+  			{'label':'Add User','url':'#/create'}
+  		];
+  		
+	    $scope.users = users.data;
+
+}]);
