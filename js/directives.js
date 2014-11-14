a0d0fd16b606c14ee5afc9f55342856bfe0ec1f8
Kevin Rice <k.l.rice@nasa.gov>
2014-11-14 17:43:38 -0500

    removed unneeded code

--- js/directives.js
+++ js/directives.js
@@ -90,119 +90,12 @@ ifpDirectives.directive('ifpSvg',['$animate',function(){
                 }
                 else if(this.type == "text"){
                     var currentPath = evDisplay.text(this.transform[4],this.transform[5],this.text);
-                    currentPath.attr({"font-family": 'Times New Roman',"font-size": '8','stroke-width': '0','stroke-opacity': '1','fill': '#000000'});
+                    currentPath.attr({"font-family": 'Arial',"font-size": '8','stroke-width': '0','stroke-opacity': '1','fill': '#000000'});
                 }   
             });
             evDisplay.setViewBox(0,0,400,300);
             evDisplay.canvas.setAttribute('preserveAspectRatio', 'xMidYMid meet');;
-            /*
             
-            $(keyplan.Hall).each(function() {
-                if(this.type == 'polygon' || this.type == 'path' || this.type == "polyline"){
-                    var currentPath = kpDisplay.path(this.points);  
-                    currentPath.attr( { "fill": hallFill, "stroke-width" : ".50", "stroke" : "#000000" } );
-                }
-                else if(this.type == "rectangle"){
-                    var currentPath = kpDisplay.rect(this.x,this.y,this.width,this.height);
-                    currentPath.attr( { "fill": hallFill, "stroke-width" : ".50", "stroke" : "#000000" } );
-                }
-                else if(this.type == "line"){
-                    var currentPath = kpDisplay.path(['M',this.x1,this.y1,'L',this.x2,this.y2]);
-                    currentPath.attr( { "fill": 'none', "stroke-dasharray" : "0", "stroke" : "#000000" } );
-                }
-                else if(this.type == "text"){
-                    var currentPath = kpDisplay.text(this.transform[4],this.transform[5],this.text);
-                    currentPath.attr({"font-family": 'Times New Roman',"font-size": '8','stroke-width': '0','stroke-opacity': '1','fill': '#000000'});
-                }   
-            });
-            
-            var that = scope.bt;
-                 
-            $(keyplan.Units).each(function() {
-                
-                if(this.type == 'polygon' || this.type == 'path' || this.type == "polyline") {
-                    var currentPath = kpDisplay.path(this.points);
-                }
-                else {              
-                    var currentPath = kpDisplay.rect(this.x,this.y,this.width,this.height);
-                }
-                
-                currentPath.size = 'regular';
-                
-                currentPath.attr(inActiveAttr); 
-            
-                var path_name = (typeof this.id == 'undefined')?'':this.id.replace(/(.*)\_/,'');
-    
-                if(path_name != '') {
-                        
-                    currentPath.id = path_name;
-                    // find this unit in the result's array by name.
-                    // if the unit's vacancy is unoccupied
-                    // make this unit clickable.
-                    var unit = that.units[path_name];
-                    
-                    if(typeof unit == 'undefined') {
-                        console.log(unit);
-                        console.log(path_name);
-                        return;
-                    }
-                    
-                    var path_instance = that;
-                    var current_path_name = unit['Unit']['MarketingName'];
-                    
-                    if (unit['Unit']['Information']['UnitLeasedStatus'] == "available")
-                    {
-                        //that.kUnits.push(currentPath);
-                        currentPath.attr(activeAttr);   
-                        
-                        if(current_path_name == that.currentUnitName && size == "thumbnail")
-                        {
-                            currentPath.attr({
-                                "fill"          : scope.bt.getConfigVal('keyplanAvailableHoverColor')
-                            }); 
-                        }
-                        else {
-                            currentPath.hover(function() {
-                                scope.hoverUnit = this.id;
-                                this.animate({"fill"    : scope.bt.getConfig().keyplanAvailableHoverColor   }, 250, ">");
-                                scope.hovered = true;
-                                
-                                scope.$apply();
-                            },function() {
-                                scope.hoverUnit = this.id;
-                                this.animate({"fill"    : scope.bt.getConfig().keyplanAvailableColor   }, 250, ">");
-                                scope.hovered = false;
-                                scope.$apply();
-                            });
-                        }
-                    }
-                    else {
-                        currentPath.hover(function() {
-                                scope.hoverUnit = this.id;
-                                scope.hovered = true;
-                                scope.$apply();
-                            },
-                            function() {
-                                scope.hoverUnit = this.id;
-                                scope.hovered = false;
-                                scope.$apply();
-                                //path_instance.onComplete(   path_instance.events.KEYPLAN_UNIT_OUT, { "unitName" : this.id, "size" : this["size"] } );
-                            }
-                        );
-                    }
-                    
-                        
-                    currentPath.click(function()
-                    {
-                        // set the building tour's current unit name
-                        path_instance.currentUnitName   = current_path_name;
-                        window.location ='#/apartment/'+current_path_name;
-                    });
-                }
-                else {
-                        currentPath.attr( { "fill": scope.bt.getConfig().keyplanDefaultColor  } );
-                }
-           });*/
         }
     };
 }]);
